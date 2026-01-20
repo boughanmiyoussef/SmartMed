cf938d14997c831e1dc648afd0152cc2b9ea7db2
Youssef <yussefboughanmy@gmail.com>
2026-01-20 20:54:35 +0100

    starting with the frontend

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask,request,render_template
-
-
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
+@app.route('/doctor/<path:filename>')
+def serve_doctor(filename):
+    doctor_folder = os.path.join(app.root_path, 'doctor')
+    return send_from_directory(doctor_folder, filename)
 
-
-
-
-
-#python main
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
